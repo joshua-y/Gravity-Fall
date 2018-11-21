b8970983c71def1fc10da16e89a1ab25841e7a2c
Joshua Young <joshuayoung@s-169-232-245-46.resnet.ucla.edu>
2018-11-20 16:13:35 -0800

    Final Version of HOTH V

---
+++ Assets/Scripts/MoveMissile.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveMissile : MonoBehaviour {
+
+    Rigidbody2D rb;
+    bool movingRight = true;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (transform.position.x > 7)
+            movingRight = false;
+        if (transform.position.x < -7)
+            movingRight = true;
+	}
+
+    void FixedUpdate()
+    {
+        if (movingRight)
+            moveRight();
+        else
+            moveLeft();
+    }
+
+    void moveRight()
+    {
+        transform.localEulerAngles = new Vector3(0f, 180f, 0f);
+        rb.velocity = new Vector2(5f, 0f);
+    }
+
+    void moveLeft()
+    {
+        transform.localEulerAngles = new Vector3(0f, 0f, 0f);
+        rb.velocity = new Vector2(-5f, 0f);
+    }
+}
